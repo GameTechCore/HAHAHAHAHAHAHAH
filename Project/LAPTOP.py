2a8efe17dbaaf4f2295fd31977166e2097a1a52b
GameTechCore <149743187+GameTechCore@users.noreply.github.com>
2023-11-03 12:36:29 +0530

    Update LAPTOP.py

--- Project/LAPTOP.py
+++ Project/LAPTOP.py
@@ -8,7 +8,7 @@ def LAPTOP():
     l=list(LAPTOP['serial_no'])
     for i in list(LAPTOP['serial_no']):
         if i==q:
-            d=[]
+            d=["8gb ram, 512 ssd, 1k amoled display, dedicated rtx 170, backlit keyboard, i5 10th GEN, WINDOWS 10","8gb ram, 1tb ssd, 1k amoled display, rtx 170, backlit keyboard, i5 11th GEN, WINDOWS 10","","","","","",""]
         else:
             print("INVALID RESPONCE")
             LAPTOP()
